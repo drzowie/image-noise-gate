61ec6d459e3ec1a2fa47305c0bf2645ef6795368
Craig DeForest <craig@deforest.org>
2021-03-23 17:33:11 -0600

    shred2d -> shred; 1D, 2D, and 3D cases implemented; only 2D tested so far

--- noisegate/tools.pyx
+++ noisegate/tools.pyx
@@ -10,63 +10,134 @@ from libc.math cimport sin, cos, sqrt, floor, ceil, round, exp, fabs
 import sys
 import copy
 
-def shred2d(
+def shred(
         source,
         size,
         step
         ):
     '''
-    shred2d - cut a 2D numpy array into a collection of smaller slices, with 
-    programmable size and overlap.  This is the Python interface to shred2d_cy,
-    which has more rigid calling convention.
+    shred - cut an N-dimensional numpy array into a collection of smaller slices, 
+    with programmable size and overlap.  
+    
+    If size and/or step are array-like objects then they must have exactly N
+    elements.  If either one is a scalar, it is copied N times.
+    
+    Limitation
+    ----------
+     In the current implementation, N must be between 1 and 4.
 
     Parameters
     ----------
-     source : 2-dimensional NDArray 
+     source : numpy NDArray 
          This is the data to shred.
-     size: tuple or list of 2 integers describing the size of each cutout
-     step: tuple or list of 2 integers describing the step between cutouts
+     size: tuple or list of describing the size of each cutout
+     step: tuple or list of describing the step between cutouts
 
     Returns
     -------
-    A 4-dimensional NDArray containing (Y,X,y,x) (capitals indexing which 
-    chunk, lowercase indexing within the chunks)
+    A 2N-dimensional NDArray with sizes (...,Y,X,...,y,x) (capitals indexing 
+    which chunk, lowercase indexing within the chunks)
 
     '''
     
-    cdef long source_xsiz = source.shape[1]
-    cdef long source_ysiz = source.shape[0]
-    cdef long chunk_x,chunk_y, x,y;
-    cdef long chunk_xi, chunk_yi
-    
-    cdef long xsiz = size[1]
-    cdef long ysiz = size[0]
-    cdef long xstep = step[1]
-    cdef long ystep = step[0]
-    
-    cdef long chunk_y_count = np.floor((source_ysiz-ysiz+1)/ystep)
-    cdef long chunk_x_count = np.floor((source_xsiz-xsiz+1)/xstep)
-    
-    output = np.empty(  [ chunk_y_count, chunk_x_count,
-                         ysiz,          xsiz
-                         ],
-                         dtype=float,   order='C'
-                        )
+    #
+    # CED - separate cases because they'll run faster, I think, than a 
+    # single general-purpose case.  But the API is unified in case I (or you)
+    # later think of a general-purpose way of doing this that is more elegant.
+    #
+    N = len(source.shape)
+    if( len(size)==1 ):
+        size = size + np.zeros(N)
+    if( len(step)==1 ):
+        step = step + np.zeros(N)
+        
+    if(len(size) != N  or  len(step) != N):
+        raise(ValueError("noisegate.tools.shred: size and step must match the input array"))
 
-    chunk_y0 = 0
-    for chunk_yi in range(chunk_y_count):
-        chunk_x0 = 0                
+    cdef long chunk_xi, chunk_yi, chunk_zi, chunk_wi
+    cdef long xstep, ystep, zstep, wstep
+    cdef long xsize, ysize, zsize, wsize
+    cdef long chunk_x0, chunk_y0, chunk_z0, chunk_w0
+        
+    if(N==1):
+        xsize = size[0]
+        xstep = step[0]
+        chunk_x_count = np.floor((source.shape[0]-xsize+1)/xstep).astype(int)
+        
+        output = np.empty([chunk_x_count, size[0]],dtype=float,order='C')
+
+        chunk_x0 = 0
         for chunk_xi in range(chunk_x_count):
-            output[ chunk_yi, chunk_xi, :, : ] = \
-                source[ chunk_y0:chunk_y0+ysiz,
-                        chunk_x0:chunk_x0+xsiz
-                        ]
+            output[ chunk_xi, : ] = \
+                source[ chunk_x0:chunk_x0+xsize ]
             chunk_x0 += xstep
-        chunk_y0 += ystep
-    return output
+        return output
+ 
+    elif(N==2):
+        xsize = size[1]
+        xstep = step[1]
+        ysize = size[0]
+        ystep = step[0]
 
+        chunk_y_count = np.floor((source.shape[0]-ysize+1)/ystep).astype(int)
+        chunk_x_count = np.floor((source.shape[1]-xsize+1)/xstep).astype(int)
 
+        output = np.empty(  [ chunk_y_count, chunk_x_count,
+                              ysize,         xsize
+                             ],
+                          dtype=float,   order='C'
+                          )
+
+        chunk_y0 = 0
+        for chunk_yi in range(chunk_y_count):
+            chunk_x0 = 0                
+            for chunk_xi in range(chunk_x_count):
+                output[ chunk_yi, chunk_xi, :, : ] = \
+                    source[ chunk_y0:chunk_y0+ysize,
+                            chunk_x0:chunk_x0+xsize
+                        ]
+                chunk_x0 += xstep
+            chunk_y0 += ystep
+        return output
     
+    elif(N==3):
+        xsize = size[2]
+        xstep = step[2]
+        ysize = size[1]
+        ystep = step[1]
+        zsize = size[0]
+        zstep = step[0]
+        
+        chunk_z_count = np.floor((source.shape[0]-zsize+1)/zstep).astype(int)
+        chunk_y_count = np.floor((source.shape[1]-ysize+1)/ystep).astype(int)
+        chunk_x_count = np.floor((source.shape[2]-xsize+1)/xstep).astype(int)
+    
+        output = np.empty(  [ chunk_z_count, chunk_y_count, chunk_x_count,
+                              zsize,         ysize,          xsize
+                             ],
+                          dtype=float,   order='C'
+                          )
+        chunk_z0 = 0
+        for chunk_z in range(chunk_z_count):
+            chunk_y0 = 0
+            for chunk_yi in range(chunk_y_count):
+                chunk_x0 = 0                
+                for chunk_xi in range(chunk_x_count):
+                    output[ chunk_zi, chunk_yi, chunk_xi, :, :, : ] = \
+                        source[ chunk_z0:chunk_z0+zsize,
+                                chunk_y0:chunk_y0+ysize,
+                                chunk_x0:chunk_x0+xsize
+                        ]
+                    chunk_x0 += xstep
+                chunk_y0 += ystep
+            chunk_z0 += zstep
+        return output
+ 
+    else:
+        raise(AssertionError("noisegate.tools.shred: N must be 1, 2, or 3"))
+            
+
+   
             
             
 
--- tests/test_01_tools.py
+++ tests/test_01_tools.py
@@ -9,13 +9,13 @@ Created on Tue Mar 16 10:59:31 2021
 import noisegate as ng
 import numpy as np
 
-def test_001_shred2d():
-    # 10x10 grid, 0 to 99 (Y,X indexing in final axis)
+def test_001_shred():
+    # 2-D: 10x10 grid, 0 to 99 (Y,X indexing in final axis)
     a = (np.mgrid[0:10,0:10].T.astype(float) * np.array([1,10])).sum(axis=2)
     print(f"a shape is {a.shape}")
     assert(a.shape==(10,10))
     # Extract 2x4 regions (Y,X), with steps of 2 in X and 3 in Y
-    b = ng.shred2d(a,[2,4],[3,2])
+    b = ng.shred(a,[2,4],[3,2])
     assert(b.shape==(3,3,2,4))
     assert( np.all( b[0,0] == 
                    np.array([[0,1,2,3],[10,11,12,13]])))
